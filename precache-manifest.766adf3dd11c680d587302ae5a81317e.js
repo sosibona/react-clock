f94bd7e7b9b4007940b46205e7c4616de3983453
sosibona <empty.tara@gmail.com>
2020-04-25 19:49:20 +0300

    Updates

---
+++ precache-manifest.766adf3dd11c680d587302ae5a81317e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b32da660f93bc120b1ec5a8df444e0f",
+    "url": "/react-clock/index.html"
+  },
+  {
+    "revision": "5f326eddb9ddcd777f76",
+    "url": "/react-clock/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "309b4d3b0bae27eb609f",
+    "url": "/react-clock/static/js/2.259b1fd6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-clock/static/js/2.259b1fd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f326eddb9ddcd777f76",
+    "url": "/react-clock/static/js/main.54303502.chunk.js"
+  },
+  {
+    "revision": "3c8f2cf34a33f77e3c05",
+    "url": "/react-clock/static/js/runtime-main.dd6316e1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-clock/static/media/logo.5d5d9eef.svg"
+  }
+]);
